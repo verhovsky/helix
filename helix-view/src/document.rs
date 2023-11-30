c8e41f635121f859ab9a521af5f9a9cceb9d4630
Pascal Kuthe <pascalkuthe@pm.me>
2023-11-30 01:19:17 +0100

    make path changes LSP spec conform
    Currently, helix implements operations which change the paths of files
    incorrectly and inconsistently. This PR ensures that we do the following
    whenever a buffer is renamed (`:move` and workspace edits)
    
    * always send did_open/did_close notifications
    * send will_rename/did_rename requests correctly
      * send them to all LSP servers not just those that are active for a
        buffer
      * also send these requests for paths that are not yet open in a buffer (if
        triggered from workspace edit).
      * only send these if the server registered interests in the path
    * autodetect language, indent, line ending, ..
    
    This PR also centralizes the infrastructure for path setting and
    therefore `:w <path>` benefits from similar fixed (but without didRename)

--- helix-view/src/document.rs
+++ helix-view/src/document.rs
@@ -1048,6 +1048,9 @@ impl Document {
         self.encoding
     }
 
+    /// sets the document path without sending events to various
+    /// observers (like LSP), in most cases `Editor::set_doc_path`
+    /// should be used instead
     pub fn set_path(&mut self, path: Option<&Path>) {
         let path = path.map(helix_stdx::path::canonicalize);
 
